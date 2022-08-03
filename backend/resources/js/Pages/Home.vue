1b648af5696c89feaae3ccd13a127c27f8618ccf
africamatji <africamatji>
2022-08-03 11:02:21 +0200

    inertia working

---
+++ backend/resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <H1>Welcome</H1>
+        <p>Hello , welcome to your first Inertia app!</p>
+        <Link href="/about">Go to about us</Link>
+    </div>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue";
+export default {
+    components: {
+        Link
+    },
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
